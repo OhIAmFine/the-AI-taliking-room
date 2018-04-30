253d60c0bcbc9773b72194fd24d2a0f1ba9fee06
OhIAmFine <845782625@qq.com>
2018-04-30 14:59:42 +0800

    [+]增加侧面栏样式

--- talking-room/src/pages/talking-room.js
+++ talking-room/src/pages/talking-room.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
+import { Avatar } from 'antd/lib';
+import 'antd/lib/avatar/style/css'
 import '../styles/talking-room.css'
 
 class TalkingRoom extends Component {
 	render () {
 		return (
 			<div className="talking">
-			<div className="side-bar">
-				<div className="personal-info"></div>
-				<div className="search"></div>
-				<div className="contact-lists"></div>
-			</div>
-			<div className="content">
-				<div className="contacter-info"></div>
-				<div className="talking-content"></div>
-				<div className="type-func">
-					<div className="record-func"></div>
+				<div className="side-bar">
+					<div className="personal-info">
+					<Avatar src="./imgs/head.jpg" className="head" />
+						<strong>HNMB</strong>
+
+					</div>
+					<div className="search"></div>
+					<div className="contact-lists"></div>
+				</div>
+				<div className="content">
+					<div className="contacter-info"></div>
+					<div className="talking-content"></div>
+					<div className="type-func">
+						<div className="record-func"></div>
+					</div>
 				</div>
 			</div>
-		</div>
 		);
 	}
 }
