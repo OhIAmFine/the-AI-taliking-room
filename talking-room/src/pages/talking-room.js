1564981d1adb84ab698139bca0e3f4d81bdc8e8a
OhIAmFine <845782625@qq.com>
2018-04-30 10:19:41 +0800

    [+]增加聊天页面

---
+++ talking-room/src/pages/talking-room.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class TalkingRoom extends Component {
+	render () {
+		<div className="side-bar">
+			<div className="personal-info"></div>
+			<div className="search"></div>
+			<div className="contact-lists"></div>
+		</div>
+		<div className="talking">
+			<div className="contacter-info"></div>
+			<div className="talking-content"></div>
+			<div className="type-func">
+				<div className="record-func"></div>
+			</div>
+		</div>
+	}
+}
