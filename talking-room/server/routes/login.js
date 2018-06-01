5c5d973a3255aae0483219836fad93d0aca9dd81
OhIAmFine <845782625@qq.com>
2018-06-01 21:20:03 +0800

    [+]修改前端界面和后端服务器

--- talking-room/server/routes/login.js
+++ talking-room/server/routes/login.js
@@ -1,42 +1,37 @@
-  
 const express = require('express');
 const router = express.Router();
 const MongoClinet = require('mongodb').MongoClient;
 const crypto = require('crypto');
 const md5 = crypto.createHash('md5');
+// const uid = require('uid');
 
-
-const url = 'mongodb://localhost:27017';
-const dbName = 'talking-room';
-const collectionName = 'users'
+const URL = 'mongodb://localhost:27017';
+const DB_NAME = 'talking-room';
+const COLLECTION_NAME = 'users';
 
 
 /* POST users listing. */
-router.post('/', function(req, res, next) {
-  console.log(req.body);
-  // 对密码进行加密存储
-  const user = Object.assign({}, req.body, {
-  	password : md5.update(req.body.password).digest('base64')
-  });
-  console.log(user);
+router.post('/', async function(req, res, next) {
+    console.log(req.body);
+    // 对密码进行加密存储
+    const user = Object.assign({}, req.body);
+    // const user = Object.assign({}, req.body, {
+    // 	password : md5.update(req.body.password).digest('base64')
+    // });
+    console.log(user);
+    const db = await connectDB(URL, DB_NAME);
+    const findResult = await findUser(db, COLLECTION_NAME, {username: user.username})
+    console.log(findResult)
   // 连接数据库
-  	MongoClinet.connect(url, async function (err, client) {
-		console.log('connect db successfully');
-		const db = await client.db(dbName);
-		const findResult = await findUser(db, collectionName, {username: user.username, password: user.password})
-		console.log(findResult)
-		if (findResult >= 1) { 
-			res.send({"success" : true, info : "登录成功" });
-			console.log("登陆成功");
-			// next();
-		}else {
-			res.send({"success" : false, info : "用户名不存在"});
-			console.log("用户名不存在");
-			// next();
-		}
-		client.close();
-		console.log('关闭数据库')
-	});
+	if (findResult.length >= 1) { 
+		res.send({"success" : true, info : "登录成功",uid: findResult.uid});
+		console.log("登陆成功");
+		// next();
+	}else {
+		res.send({"success" : false, info : "用户名不存在"});
+		console.log("用户名不存在");
+		// next();
+	}
 });
 
 
@@ -50,7 +45,16 @@ async function insertUserInfo (db, collectionName, doc) {
 // 查询用户方法
 async function findUser (db, collectionName,query) {
 	const collection = db.collection(collectionName);
-	const  result  = await collection.find(query).count()
-	return result;
+	// console.log('collection', collection)
+	const  arr  = await collection.find(query).toArray();
+	return {uid: arr[0].uid, length: arr.length};
 }
+
+async function connectDB(url, dbName) {
+	console.log('ready to connect db');
+	const client = await MongoClinet.connect(url);
+	const db =   client.db(dbName)
+	return db;
+}
+
 module.exports = router;
--- talking-room/server/routes/register.js
+++ talking-room/server/routes/register.js
@@ -3,40 +3,39 @@ const router = express.Router();
 const MongoClinet = require('mongodb').MongoClient;
 const crypto = require('crypto');
 const md5 = crypto.createHash('md5');
+const uid = require('uid')
 
-
-const url = 'mongodb://localhost:27017';
-const dbName = 'talking-room';
-const collectionName = 'users'
+const URL = 'mongodb://localhost:27017';
+const DB_NAME = 'talking-room';
+const COLLECTION_NAME = 'users';
 
 
 /* POST users listing. */
-router.post('/', function(req, res, next) {
-  console.log(req.body);
-  // 对密码进行加密存储
-  const user = Object.assign({}, req.body, {
-  	password : md5.update(req.body.password).digest('base64')
-  });
-  console.log(user);
-  // 连接数据库
-  	MongoClinet.connect(url, async function (err, client) {
-		console.log('connect db successfully');
-		const db = await client.db(dbName);
-		const findResult = await findUser(db, collectionName, {username: user.username, password: user.password})
-		if (findResult > 1) { 
-			res.send({"success" : false, info : "您已经注册过了" });
-			console.log("您已经注册过了");
-			// next();
-		}else {
-			const inseltResult = await insertUserInfo(db, collectionName, user);
-			console.log(inseltResult);
-			// 插入用户
-			res.send({"success" : true, info : "注册成功"});
-			console.log("注册成功");
-			// next();
-		}
-		client.close();
+router.post('/', async function(req, res, next) {
+	console.log(req.body);
+	// 对密码进行加密存储
+	// const user = Object.assign({}, req.body, {
+	// 	password : md5.update(req.body.password).digest('base64')
+	// });
+	const user = Object.assign({}, req.body, {
+		uid: uid(10)
 	});
+	console.log(user);
+	// 连接数据库
+    const db = await connectDB(URL, DB_NAME);
+    const findResult = await findUser(db, COLLECTION_NAME, {username: user.username, password: user.password})
+	if (findResult > 1) { 
+		res.send({"success" : false, info : "您已经注册过了" });
+		console.log("您已经注册过了");
+		// next();
+	}else {
+		const inseltResult = await insertUserInfo(db, COLLECTION_NAME, user);
+		console.log(inseltResult);
+		// 插入用户
+		res.send({"success" : true, info : "注册成功", id : user.uid});
+		console.log("注册成功");
+		// next();
+	}
 });
 
 
@@ -52,5 +51,12 @@ async function findUser (db, collectionName,query) {
 	const collection = db.collection(collectionName);
 	const  result  = await collection.find(query).count()
 	return result;
+}
+
+async function connectDB(url, dbName) {
+	console.log('ready to connect db');
+	const client = await MongoClinet.connect(url);
+	const db =   client.db(dbName)
+	return db;
 }
 module.exports = router;
